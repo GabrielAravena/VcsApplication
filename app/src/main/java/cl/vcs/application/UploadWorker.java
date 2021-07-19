1fcc4f0b2f309ec266144f59d1f1119f85b9e92d
Gabriel Aravena <gabriel.aravena@apoyate.cl>
2021-07-19 08:22:22 -0400

    Se soluciona url de toma de estado

--- app/src/main/java/cl/vcs/application/UploadWorker.java
+++ app/src/main/java/cl/vcs/application/UploadWorker.java
@@ -148,7 +148,7 @@ public class UploadWorker extends Worker {
 
             Log.e("JSON_TOMA", jsonBody.toString().replaceAll("ñ", "n").replaceAll("Ñ", "N").replaceAll("Á", "A"));
 
-            String url = "https://apimovil.vrrd.cl/api/TomaEstad";
+            String url = "https://apimovil.vrrd.cl/api/TomaEstado";
 
             HostnameVerifier hostnameVerifier = new HostnameVerifier() {
                 @Override
